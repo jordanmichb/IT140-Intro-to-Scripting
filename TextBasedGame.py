d458d5e26ccdb138e05ae75917f2c0fc31e746f8
jordanmichb <95947696+jordanmichb@users.noreply.github.com>
2021-12-11 17:47:31 -0800

    Update TextBasedGame.py

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -10,14 +10,20 @@ def show_instructions():
 
 
 def show_status(current_room, inventory, rooms):
-    print("You are at the " + current_room + ".\n")  # show current room
-    print("Inventory: {}\n".format(inventory))  # display inventory
-    if 'Item' in rooms[current_room]:  # determines if room has an item
-        if rooms[current_room]['Item'] not in inventory:  # determines if item has been picked up
+    # Show current room.
+    print("You are at the " + current_room + ".\n")
+    # Display inventory.
+    print("Inventory: {}\n".format(inventory)) 
+    # Determines if room has an item.
+    if 'Item' in rooms[current_room]:  
+        # Determines if item has been picked up.
+        if rooms[current_room]['Item'] not in inventory:  
             if current_room == 'Cave':
-                print("You see an " + rooms[current_room]['Item'] + ".\n")  # separate text for item starting with noun
+                # Use separate text for item starting with noun.
+                print("You see an " + rooms[current_room]['Item'] + ".\n")  
             elif current_room == 'Waterfall':
-                print("You see some " + rooms[current_room]['Item'] + ".\n")  # separate text for plural item
+                # Use separate text for plural item.
+                print("You see some " + rooms[current_room]['Item'] + ".\n")  # separate text for plural item.
             else:
                 print("You see a " + rooms[current_room]['Item'] + ".\n")
 
@@ -34,35 +40,47 @@ def main():
         'Waterfall': {'West': 'Riverbank', 'Item': 'Silver Bullets'},
         'Clearing': {'South': 'Redwood Grove'}
     }
-
-    current_room = 'Campsite'  # start at campsite
-
-    show_instructions()  # call function
-
-    while current_room != 'Clearing':  # true for all rooms except clearing
-        show_status(current_room, inventory, rooms)  # call function
-        command = input('Enter a command: ').title()  # normalize input so program reads any caps combo and matches dict
+    
+    # Start at campsite.
+    current_room = 'Campsite'  
+    # Call function.
+    show_instructions()  
+    
+    # True for all rooms except clearing.
+    while current_room != 'Clearing':
+        # Call function.  
+        show_status(current_room, inventory, rooms)
+        # Normalize input so program reads any caps combo and matches dict.
+        command = input('Enter a command: ').title()  
         print("--------------------------------------")
-        if command.startswith("Get"):  # determines if command is to pick up item
-            if command[4:] in inventory:  # determines if command refers to any item already retrieved, excludes 'Get '
+        # Determines if command is to pick up item.
+        if command.startswith("Get"):  
+            # Determines if command refers to any item already retrieved, excludes 'Get '.
+            if command[4:] in inventory:  
                 print("You already have that!\n")
-            elif command[4:] == rooms[current_room]['Item']:  # disallows extra input characters after item
-                inventory.append(rooms[current_room]['Item'])  # add item
+                # Disallows extra input characters after item
+            elif command[4:] == rooms[current_room]['Item']:  
+                # Add item.
+                inventory.append(rooms[current_room]['Item'])  
                 print(rooms[current_room]['Item'], "retrieved!\n")
             else:
                 print("Can't " + command.lower() + ".\n")
-        elif command in rooms[current_room]:  # determines if command is to move rooms
-            current_room = rooms[current_room][command]  # updates room if valid command
+        # Determines if command is to move rooms.
+        elif command in rooms[current_room]:  
+            # Updates room if valid command.
+            current_room = rooms[current_room][command]  
         else:
             print("You can't do that!\n")
 
     if current_room == 'Clearing':
-        if len(inventory) < 6:  # if player did not collect all items
+        # If player did not collect all items.
+        if len(inventory) < 6:  
             print("You have made it to the clearing at the edge of the woods...\n")
             print("You see the Wendigo, and the Wendigo sees you...\n")
             print("You frantically search your pockets and realize you are missing supplies!\n")
             print("The Wendigo has killed you.")
-        elif len(inventory) == 6:  # if player collected all items
+        # If player collected all items.
+        elif len(inventory) == 6:  
             print("You have made it to the clearing at the edge of the woods...\n")
             print("You see the Wendigo, and the Wendigo sees you...\n")
             print("You gathered all the supplies and were able to defeat the Wendigo!\n")
